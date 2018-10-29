5f08a7710361356dcb7d064516b3c275600a27f4
jlx2002387 <jlx2002387@163.com>
2018-10-29 14:37:44 +0800

    Initial commit

---
+++ app/src/main/java/com/czbix/smbsteamer/AppCtx.java
@@ -0,0 +1,35 @@
+package com.czbix.smbsteamer;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.czbix.smbsteamer.helper.PreferenceHelper;
+import com.czbix.smbsteamer.util.SmbUtils;
+import com.czbix.smbsteamer.util.Utils;
+import com.danikula.videocache.HttpProxyCacheServer;
+
+public class AppCtx extends Application {
+    private HttpProxyCacheServer proxy;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initPrefs();
+        initSmb();
+    }
+    private void initPrefs() {
+        PreferenceHelper.getInstance().init(this);
+    }
+    private void initSmb() {
+        SmbUtils.init();
+    }
+    public static HttpProxyCacheServer getProxy(Context context) {
+        AppCtx app = (AppCtx) context.getApplicationContext();
+        return app.proxy == null ? (app.proxy = app.newProxy()) : app.proxy;
+    }
+    private HttpProxyCacheServer newProxy() {
+        return new HttpProxyCacheServer.Builder(this)
+                //.maxCacheSize(1024*1024*64)
+                .cacheDirectory(Utils.getVideoCacheDir(this))
+                .build();
+    }
+}
